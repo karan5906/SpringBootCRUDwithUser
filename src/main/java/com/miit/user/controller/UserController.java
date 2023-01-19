f6bf11bb896b953b360f15525e41421db1cbd6d7
karan5906 <karanc5906@gmail.com>
2023-01-19 00:14:22 -0500

    Spring Boot Test 01 -Karan

--- src/main/java/com/miit/user/controller/UserController.java
+++ src/main/java/com/miit/user/controller/UserController.java
@@ -17,6 +17,10 @@ import org.springframework.web.client.RestTemplate;
 import com.miit.user.entites.User;
 import com.miit.user.services.UserServices;
 
+/**
+ * @author 19059 - Spring Boot Test 01 -Karan
+ *
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
